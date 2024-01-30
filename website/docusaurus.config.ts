95a6e61fbfedcc1c2a416c6a1990240364e10a70
Aytana18 <neiramonjeaytana@gmail.com>
2024-01-30 14:03:15 -0500

    Cambio de logo miniaturo

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -18,8 +18,8 @@ const config: Config = {
   organizationName: 'Aytana18', 
   projectName: 'Facturacion', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: 'log',
+  onBrokenMarkdownLinks: 'log',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
